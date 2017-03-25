a81caea409f881298aaecc85cabc9a1aa5b067e0
baratimur <bara.timur@live.com>
2017-03-25 21:33:00 +0700

    Done implementation example for piko

--- app/src/main/java/com/gwk/pikodoveexample/MainActivity.java
+++ app/src/main/java/com/gwk/pikodoveexample/MainActivity.java
@@ -1,13 +1,123 @@
 package com.gwk.pikodoveexample;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.gwk.pikodove.generator.PikoGenerator;
+import com.gwk.pikodove.generator.PikoGeneratorBlueprint;
+import com.gwk.pikodove.parser.PikoParser;
+import com.gwk.pikodove.parser.PikoParserBlueprint;
+
+import org.json.JSONException;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnShowObjectRepresentation;
+    private Button btnShowJsonRepresentation;
+    private Button btnShowPikoRepresentation;
+    private Button btnTest;
+    private TextView tvResult;
+
+    private User originalUser;
+    private byte[] pikoRepresentation;
+    private User parsedUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        originalUser = new User();
+        originalUser.setByDefaultValue();
+
+        try {
+            pikoRepresentation = PikoGenerator.fromClass(originalUser, new PikoGeneratorBlueprint(User.class));
+            PikoParserBlueprint pikoParserBlueprint = new PikoParserBlueprint(User.class);
+            parsedUser = (User) PikoParser.fromPiko(pikoRepresentation, pikoParserBlueprint);
+        } catch (IllegalAccessException | InstantiationException | IOException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        btnShowObjectRepresentation = (Button) findViewById(R.id.btnShowObjectRepresentation);
+        btnShowJsonRepresentation = (Button) findViewById(R.id.btnShowJsonRepresentation);
+        btnShowPikoRepresentation = (Button) findViewById(R.id.btnShowPikoRepresentation);
+        btnTest = (Button) findViewById(R.id.btnTest);
+        tvResult = (TextView) findViewById(R.id.textViewResult);
+        initListeners();
+    }
+
+    private void initListeners() {
+        btnShowObjectRepresentation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this).setMessage(originalUser.toString()).show();
+            }
+        });
+        btnShowJsonRepresentation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this).setMessage(originalUser.toJsonObject()).show();
+            }
+        });
+        btnShowPikoRepresentation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this).setMessage(new String(pikoRepresentation)).show();
+            }
+        });
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // json
+                int lengthJson = originalUser.toJsonObject().length();
+                String result = "Json Data Length : " + lengthJson;
+                long startTime = System.nanoTime();
+                try {
+                    new User().fromJsonObject(originalUser.toJsonObject());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                long endTime = System.nanoTime();
+                long durationJson = (endTime - startTime) / 1000;
+                result += "\nJson Data Parse Time : " + durationJson + " ms";
+
+                // piko
+                int lengthPiko = pikoRepresentation.length;
+                result += "\nPiko Data Length : " + lengthPiko;
+                PikoParserBlueprint pikoParserBlueprint = new PikoParserBlueprint(User.class);
+                startTime = System.nanoTime();
+                try {
+                    parsedUser = (User) PikoParser.fromPiko(pikoRepresentation, pikoParserBlueprint);
+                } catch (IllegalAccessException | IOException | NoSuchFieldException | InstantiationException e) {
+                    e.printStackTrace();
+                }
+                endTime = System.nanoTime();
+                long durationPiko = (endTime - startTime) / 1000;
+                result += "\nPiko Data Parse Time : " + durationPiko + " ms";
+                result += "\nLength percentage : " + (((float)(lengthJson - lengthPiko) / (float)lengthJson) * 100f) + "%";
+                result += "\nDuration percentage : " + (((float)(durationJson - durationPiko) / (float)durationJson) * 100f) + "%";
+                tvResult.setText(result);
+            }
+        });
+
+    }
+
+
+    String toBinary(byte[] bytes)
+    {
+        StringBuilder sb = new StringBuilder(bytes.length * Byte.SIZE);
+        for( int i = 0; i < Byte.SIZE * bytes.length; i++ ) {
+            if(i % 8 == 0 && i != 0) {
+                sb.append(" ");
+            }
+            sb.append((bytes[i / Byte.SIZE] << i % Byte.SIZE & 0x80) == 0 ? '0' : '1');
+        }
+        return sb.toString();
     }
 }
---
+++ app/src/main/java/com/gwk/pikodoveexample/User.java
@@ -0,0 +1,97 @@
+package com.gwk.pikodoveexample;
+
+import com.gwk.pikodove.annotation.PikoFixedLength;
+import com.gwk.pikodove.annotation.PikoString;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * User Model to demonstrate data parser
+ * Created by Zavyra on 2017-03-25.
+ */
+
+public class User {
+    public boolean isAdmin;
+    public byte accessLevel;
+    public short type;
+    public int currentBalance;
+    public long totalPoint;
+    public char initial;
+    public float avgBalance;
+    public double avgPoint;
+    @PikoFixedLength(6) public String kodepos;
+    @PikoString('\n') public String address;
+    @PikoString public String name;
+
+    public User() {
+
+    }
+
+    public void setByDefaultValue() {
+        this.isAdmin = true;
+        this.accessLevel = 0x12;
+        this.type = 41;
+        this.currentBalance = 123431;
+        this.totalPoint = 123456789098L;
+        this.initial = 'c';
+        this.avgBalance = 2.5f;
+        this.avgPoint = 203040.50607D;
+        this.kodepos = "123456";
+        this.address = "Bandung Kota";
+        this.name = "Garuda Wisnu Kencana";
+    }
+
+    public void fromJsonObject(String jsonObject) throws JSONException {
+        JSONObject j = new JSONObject(jsonObject);
+        this.isAdmin = j.getBoolean("isAdmin");
+        this.accessLevel = (byte) j.getInt("accessLevel");
+        this.type = (short) j.getInt("type");
+        this.currentBalance = j.getInt("currentBalance");
+        this.totalPoint = j.getLong("totalPoint");
+        this.initial = (char) j.getInt("initial");
+        this.avgBalance = (float) j.getDouble("avgBalance");
+        this.avgPoint = j.getDouble("avgPoint");
+        this.kodepos = j.getString("kodepos");
+        this.address = j.getString("address");
+        this.name = j.getString("name");
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("isAdmin : ").append(isAdmin ? "true" : "false");
+        sb.append("\naccessLevel : ").append(accessLevel);
+        sb.append("\ntype : ").append(type);
+        sb.append("\ncurrentBalance : ").append(currentBalance);
+        sb.append("\ntotalPoint : ").append(totalPoint);
+        sb.append("\ninitial : ").append(initial);
+        sb.append("\navgBalance : ").append(avgBalance);
+        sb.append("\navgPoint : ").append(avgPoint);
+        sb.append("\nkodepos : ").append(kodepos);
+        sb.append("\naddress : ").append(address);
+        sb.append("\nname : ").append(name);
+        return sb.toString();
+    }
+
+    public String toJsonObject() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("isAdmin",isAdmin);
+            jsonObject.put("accessLevel",accessLevel);
+            jsonObject.put("type",type);
+            jsonObject.put("currentBalance",currentBalance);
+            jsonObject.put("totalPoint",totalPoint);
+            jsonObject.put("initial",initial);
+            jsonObject.put("avgBalance",avgBalance);
+            jsonObject.put("avgPoint",avgPoint);
+            jsonObject.put("kodepos",kodepos);
+            jsonObject.put("address",address);
+            jsonObject.put("name",name);
+            return jsonObject.toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
